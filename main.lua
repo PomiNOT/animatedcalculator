db4305f0eac4a20d232bbc7b831373ab4e12206f
NOT <ppminet@protonmail.com>
2021-05-27 10:15:32 +0700

    minimap

--- main.lua
+++ main.lua
@@ -10,7 +10,14 @@ function love.load()
   typingFont = love.graphics.newFont(16)
   renderList = {}
   currentStepIndex = 1
-  animationDuration = 0.3
+  animationDuration = 0.2
+  camX, camY = 0, 0
+  camX0, camY0 = 0, 0
+  mouseX0, mouseY0 = 0, 0
+  mouseDown = false
+  camZoom = 1
+
+  love.window.setTitle("Bruh")
 
   reset("5+5*10")
 end
@@ -40,6 +47,52 @@ crons = {}
 function love.update(dt)
   flux.update(dt)
   for _, c in pairs(crons) do c:update(dt) end
+
+  if not mouseDown then
+    mouseX0, mouseY0 = love.mouse.getPosition()
+    camX0, camY0 = camX, camY
+  end
+
+  mouseDown = love.mouse.isDown(1)
+
+  if mouseDown then
+    local mouseX, mouseY = love.mouse.getPosition()
+    local dMouseX, dMouseY = mouseX - mouseX0, mouseY - mouseY0
+    camX, camY = camX0 - dMouseX / camZoom, camY0 - dMouseY / camZoom
+  end
+end
+
+function mouseToWorld()
+  local mouseX, mouseY = love.mouse.getPosition()
+  local mouseWorldX = mouseX / camZoom + camX
+  local mouseWorldY = mouseY / camZoom + camY
+  return mouseWorldX, mouseWorldY
+end
+
+function love.mousepressed(x, y, btn)
+  if btn == 2 then
+    camX = 0
+    camY = 0
+    camZoom = 1
+  end
+end
+
+function love.wheelmoved(x, y)
+  local wMouseX0, wMouseY0 = mouseToWorld()
+
+  if y > 0 then
+    camZoom = camZoom + 0.1
+  elseif y < 0 then
+    camZoom = camZoom - 0.1
+    if camZoom < 0.1 then
+      camZoom = 0.1
+    end
+  end
+
+  local wMouseX, wMouseY = mouseToWorld()
+  local dx, dy = wMouseX -wMouseX0, wMouseY - wMouseY0
+
+  camX, camY = camX - dx, camY - dy
 end
 
 function makeRenderListForStep(step)
@@ -135,36 +188,37 @@ function nextStep(delta)
   --Do animation
   for _, index in pairs(changes.removed) do
     flux.to(renderList[index], animationDuration, { opacity = 0, y = 10 })
-
-    --After removing animation is done
-    local c1 = cron.after(animationDuration, function ()
-      for _, moved in pairs(changes.moved) do
-        flux
-          .to(renderList[moved.index], animationDuration, { x = moved.location.x, y = moved.location.y })
-      end
-    end)
-
-    --After moving animation is done
-    local c2 = cron.after(animationDuration * 2, function ()
-      skipAllowed = true
-
-      for _, added in pairs(changes.added) do
-        renderList = nextStepRenderList
-        renderList[added].opacity = 0
-        renderList[added].y = -10
-        flux.to(renderList[added], animationDuration, { opacity = 1, y = 0 })
-      end
-    end)
-
-    table.insert(crons, c1)
-    table.insert(crons, c2)
   end
+
+  --After removing animation is done
+  local c1 = cron.after(animationDuration, function ()
+    for _, moved in pairs(changes.moved) do
+      flux
+        .to(renderList[moved.index], animationDuration, { x = moved.location.x, y = moved.location.y })
+    end
+  end)
+
+  --After moving animation is done
+  local c2 = cron.after(animationDuration * 2, function ()
+    skipAllowed = true
+
+    for _, added in pairs(changes.added) do
+      renderList = nextStepRenderList
+      renderList[added].opacity = 0
+      renderList[added].y = -10
+      flux.to(renderList[added], animationDuration, { opacity = 1, y = 0 })
+    end
+  end)
+
+  table.insert(crons, c1)
+  table.insert(crons, c2)
 
   currentStepIndex = currentStepIndex + delta
 end
 
 function drawInput()
   love.graphics.setFont(typingFont)
+  love.graphics.setColor(1, 1, 1, 1)
 
   local sw, sh = love.graphics.getDimensions()
   local keyboard_input_string = table.concat(keyboard_input, "")
@@ -181,14 +235,36 @@ function drawRenderList()
   for i, renderElement in pairs(renderList) do
     local text = tostring(renderElement.token.value)
     local o = renderElement.opacity
-    love.graphics.setColor(o, o, o, 1)
+    love.graphics.setColor(o, o, o, o)
 
     love.graphics.print(text, renderElement.x, renderElement.y)
   end
 end
 
 function love.draw()
-  drawInput()
+  love.graphics.scale(camZoom)
+  love.graphics.translate(-camX, -camY)
+
   drawRenderList()
+
+  local w, h = love.graphics.getDimensions()
+  local x, y = mouseToWorld()
+
+  love.graphics.origin()
+  love.graphics.scale(0.25)
+
+  love.graphics.setColor(1, 1, 1, 0.2)
+  love.graphics.rectangle("line", 0, 0, w, h)
+
+  love.graphics.setColor(1, 0, 0, 1)
+  love.graphics.rectangle("line", camX, camY, w / camZoom, h / camZoom)
+
+  love.graphics.setColor(1, 1, 1, 1)
+  love.graphics.circle("fill", x, y, 5)
+
+  drawRenderList()
+
+  love.graphics.origin()
+  drawInput()
 end
 
